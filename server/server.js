61f82e6af8727acf1d0994abedfb21f083b8d706
akhilles <akhilvelagapudi@gmail.com>
2017-04-29 20:30:13 -0400

    added required libraries and unique ID handling

--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+const express = require('express');
+const parser = require('body-parser');
+const hasher = require('password-hash');
+const crypto = require('crypto');
+const mysql = require('mysql');
 
+const app = express();
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+const pool = mysql.createPool({
+	connectionLimit: 5,
+	host: 'hotelreservationdb.c3all2cpsbip.us-east-1.rds.amazonaws.com',
+	user: 'archimedes',
+	password: 'bogey-shoo-flair',
+	database: 'hotelreservationdb',
+	debug: true
+});
 
+function generateUniqueID(table, column, next){
+	const id = crypto.randomBytes(5).toString("hex");
+	pool.query('SELECT * FROM ? WHERE ?=?', [table, column, id], function(err, results, fields){
+		if (err){
+			return next(err);
+		}
+		if (results.length == 0){
+			return next(false, id);
+		} else {
+			generateUniqueID(table, column, next);
+		}
+	});
+}
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, function(){
 	console.log("hotel_reservation_api listening on port " + port);
 });
